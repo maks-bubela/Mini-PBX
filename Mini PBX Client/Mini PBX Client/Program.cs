2a6e020f66157bc37a2ab8c0ce81648c4fd18a3e
Oleksandr Lusenko <60522005+Lusenko@users.noreply.github.com>
2021-02-15 15:42:32 +0200

    Added Mini PBX Client

---
+++ Mini PBX Client/Mini PBX Client/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Mini_PBX_Client
+{
+    class Program
+    {
+        static string phone_number;
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свой номер: ");
+            phone_number = Console.ReadLine();
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port); 
+                stream = client.GetStream(); 
+
+                string message = phone_number;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start(); 
+                Console.WriteLine("Ваш номер: {0}", phone_number);
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+       
+        static void SendMessage()
+        {
+            Console.Write("Введите номер: ");
+            string message = Console.ReadLine();
+            message = $"Вызов {message} ...";
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            while (true)
+            {
+                message = Console.ReadLine();
+                data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+       
+        static void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("Конец!"); 
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();
+            if (client != null)
+                client.Close();
+            Environment.Exit(0); 
+        }
+    }
+}
