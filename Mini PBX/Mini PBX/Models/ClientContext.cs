9571a5471fc22fdb3d39821c70907c3bac472806
maks-bubela <ya.bubela@gmail.com>
2021-02-24 16:41:19 +0300

    Add Context file to project

---
+++ Mini PBX/Mini PBX/Models/ClientContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Mini_PBX.Models
+{
+    public class ClientContext : DbContext
+    {
+        public ClientContext()
+            : base ("name=ClientContext")
+        {
+        }
+    }
+}
