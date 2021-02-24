fa01871f9871a16b96e251744b6f6e9fd677b600
maks-bubela <ya.bubela@gmail.com>
2021-02-24 19:53:09 +0300

    Added restrictions in ClientDTO file

--- Mini PBX Server/Mini PBX Server/Model/ClientDTO.cs
+++ Mini PBX Server/Mini PBX Server/Model/ClientDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Mini_PBX.Models
@@ -7,7 +8,11 @@ namespace Mini_PBX.Models
     class ClientDTO
     {
         public long Id { get; set; }
+        [MaxLength(256)]
+        [Required]
         public string userName { get; set; }
+        [Required]
+        [MaxLength(3)]
         public string phone_number { get; set; }
     }
 }
