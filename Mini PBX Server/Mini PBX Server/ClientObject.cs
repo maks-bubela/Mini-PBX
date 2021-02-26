d908680d94844217bb2bd3a0a89ef68ed99fb9ff
maks-bubela <ya.bubela@gmail.com>
2021-02-26 14:14:43 +0300

    Remake Process method for work through by ClientDTO

--- Mini PBX Server/Mini PBX Server/ClientObject.cs
+++ Mini PBX Server/Mini PBX Server/ClientObject.cs
@@ -26,42 +26,54 @@ namespace Mini_PBX
         public void Process()
         {
             string message = "";
+            string call_numb = "";
+            string userName = "";
             try
             {
-                string call_numb="";
                 Stream = client.GetStream();
                 // Get phone number
                 message = GetMessage();
-                phone_number = message;
-                message = phone_number + ": На линии ";
-                Console.WriteLine(message);
-
-                message = GetMessage();
-                message = phone_number + ": " + message;
-                for(int i = 5; i < message.Length; i++)
+                for (int i = 0; i < message.Length; i++)
                 {
-                    if(message[i] >='0' && message[i] <= '9')
-                         call_numb += message[i];
+                    if (message[i] >= '0' && message[i] <= '9')
+                        phone_number += message[i];
+                    else if ((message[i] >= 'a' && message[i] <= 'z') || (message[i] >= 'A' && message[i] <= 'Z'))
+                        userName += message[i];
                 }
-                server.CheckAndConect(call_numb, this);
-                Console.WriteLine(message);
-                bool check = true;
-                while (check)
+                if (phone_number.Length == 3)
                 {
-                    try
+                    if (!clientLogin(phone_number, userName)) { }
+                        clientRegistration(phone_number, userName);
+                    message = phone_number + ": На линии ";
+                    Console.WriteLine(message);
+
+                    message = GetMessage();
+                    message = phone_number + ": " + message;
+                    for(int i = 5; i < message.Length; i++)
                     {
-                        message = GetMessage();
-                        message = String.Format("{0}: {1}", phone_number, message);
-                        Console.WriteLine(message);
-                        server.BroadcastMessage(message, this.phone_number);
+                        if(message[i] >='0' && message[i] <= '9')
+                             call_numb += message[i];
                     }
-                    catch
+                    server.CheckAndConect(call_numb, this);
+                    Console.WriteLine(message);
+                    bool check = true;
+                    while (check)
                     {
-                        message = String.Format("{0}: Сбросил", phone_number);
-                        Console.WriteLine(message);
-                        server.BroadcastMessage(message, this.phone_number);
-                        server.RemoveCall(this);
-                        check = false;
+                        try
+                        {
+                            message = GetMessage();
+                            message = String.Format("{0}: {1}", phone_number, message);
+                            Console.WriteLine(message);
+                            server.BroadcastMessage(message, this.phone_number);
+                        }
+                        catch
+                        {
+                            message = String.Format("{0}: Сбросил", phone_number);
+                            Console.WriteLine(message);
+                            server.BroadcastMessage(message, this.phone_number);
+                            server.RemoveCall(this);
+                            check = false;
+                        }
                     }
                 }
             }
