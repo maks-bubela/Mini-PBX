215a8ed268e26fe189977309ba75eb4247565401
Maks Bubela <ya.bubela@gmail.com>
2021-03-04 16:38:06 +0300

    Add ClientConfiguration class for set db settings and propertys

---
+++ Mini PBX Server/Mini PBX Server/Model/ClientConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mini_PBX.Models;
+
+namespace Mini_PBX_Server.Model
+{
+    public class ClientConfiguration : EntityTypeConfiguration<ClientDTO>
+    {
+        public ClientConfiguration()
+        {
+            ToTable("ClientDTOes")
+                 .HasKey(u => u.Id)
+                 .Property(u => u.Id)
+                 .IsRequired();
+            Property(u => u.phone_number)
+                .IsRequired()
+                .HasColumnType("smallint")
+                .HasMaxLength(3);
+            Property(u => u.userName)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(256);
+        }
+    }
+}
