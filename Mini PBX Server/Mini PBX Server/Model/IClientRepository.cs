24a9705f98394f73cd2906b3f1f2d103658474df
Maks Bubela <ya.bubela@gmail.com>
2021-03-05 10:57:06 +0300

    Add ClientRepository interface

---
+++ Mini PBX Server/Mini PBX Server/Model/IClientRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mini_PBX.Models;
+
+namespace Mini_PBX_Server.Model
+{
+    public interface IClientRepository
+    {
+        string GetClientUserName(string phone_number);
+        string GetClientPhoneNumber(string userName);
+        ClientDTO GetClient(string phone_number, string userName);
+        void clientRegistration(string phone_number, string userName);
+        bool clientLogin(string phone_number, string userName);
+
+    }
+}
