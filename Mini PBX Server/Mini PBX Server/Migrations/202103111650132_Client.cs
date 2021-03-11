29cc00657a0605b930c1d366ecbab7d27c6f6462
Maks Bubela <ya.bubela@gmail.com>
2021-03-11 19:56:24 +0300

    Do new migration

---
+++ Mini PBX Server/Mini PBX Server/Migrations/202103111650132_Client.cs
@@ -0,0 +1,18 @@
+﻿namespace Mini_PBX_Server.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Client : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.ClientDTOes", newName: "Client");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.Client", newName: "ClientDTOes");
+        }
+    }
+}
