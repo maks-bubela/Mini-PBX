10adc843fb753b58dd5c62d937918e6e14c642da
Maks Bubela <ya.bubela@gmail.com>
2021-03-16 15:09:17 +0300

    Change implementation for methods

--- Mini PBX Server/Mini PBX Server/Model/ClientRepository.cs
+++ Mini PBX Server/Mini PBX Server/Model/ClientRepository.cs
@@ -11,35 +11,15 @@ namespace Mini_PBX_Server.Model
     {
         public Client GetClient(string phone_number,string userName)
         {
-            Client client = new Client();
-            var clients = new ClientContext().client
-                       .Where(c => c.phone_number == phone_number)
-                       .Where(c => c.userName == userName).ToList();
-            if (clients.Count > 0)
-            {
-                client.Id = clients[0].Id;
-                client.phone_number = clients[0].phone_number;
-                client.userName = clients[0].userName;
-                return client;
-            }
-            return null;
+            return new ClientContext().client.FirstOrDefault(u => IsClientExist(phone_number, userName));
         }
-
-
         public void AddClientToDataBase(string phone_number, string userName)
         {
-            ClientContext context = new ClientContext();
-            Client newClient = new Client();
-            newClient.phone_number = phone_number;
-            newClient.userName = userName;
-            context.client.Add(newClient);
-            context.SaveChanges();
+            new ClientContext(new Client(phone_number, userName));
         }
         public bool IsClientExist(string phone_number, string userName)
         {
-            if (new ClientContext().client.Where(o => o.phone_number == phone_number).Any(o => o.userName == userName))
-                return true;
-            return false;
+            return new ClientContext().client.Where(o => o.phone_number == phone_number).Any(o => o.userName == userName);
         }
     }
 }
