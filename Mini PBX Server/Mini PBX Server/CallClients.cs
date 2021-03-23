b9ec801467e0f9d21dd51d33a36760e99f80fe44
Maks Bubela <ya.bubela@gmail.com>
2021-03-23 13:42:15 +0300

    Add conctructor, new function checkWaitCall and new variable wait call

--- Mini PBX Server/Mini PBX Server/CallClients.cs
+++ Mini PBX Server/Mini PBX Server/CallClients.cs
@@ -5,6 +5,12 @@ namespace Mini_PBX
     {
         ClientObject client_1;
         ClientObject client_2;
+        string wait_number;
+        public CallClients(ClientObject client_1, string wait_number)
+        {
+            this.client_1 = client_1;
+            this.wait_number = wait_number;
+        }
         public CallClients(ClientObject client_1, ClientObject client_2)
         {
             this.client_1 = client_1;
@@ -12,9 +18,17 @@ namespace Mini_PBX
         }
         public ClientObject GetFirstClient() { return this.client_1; }
         public ClientObject GetSecondClient() { return this.client_2; }
+        public void SetFirstClient(ClientObject client_1) { this.client_1 = client_1; }
+        public void SetSecondClient(ClientObject client_2) {this.client_2 = client_2; }
         public bool checkClient(ClientObject client)
         {
             if (this.client_1 == client || this.client_2 == client)
+                return true;
+            return false;
+        }
+        public bool checkWaitCall(string wait_number)
+        {
+            if (this.wait_number == wait_number)
                 return true;
             return false;
         }
